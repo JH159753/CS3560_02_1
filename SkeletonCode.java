1525ccb6dd87193a0bd6207e830f30f2f25b59e9
Alex Hernandez <alexish4@cpp.edu>
2021-10-08 19:59:55 -0700

    Added Alex Changes

--- SkeletonCode.java
+++ SkeletonCode.java
@@ -4,6 +4,7 @@ public class SkeletonCode
     double withdrawAmount;
     double totalFunds;
     double remainingFunds = totalFunds - withdrawAmount;
+    double depositAmount;
 
     public static void main(String[] args)
     {
@@ -34,4 +35,22 @@ public class SkeletonCode
         /* Nothing is returned since the user is recieveing a summary of their activities*/
         /* printReciept would show the user every transaction they completed through the ATM Subsytem*/
     }
+
+
+    public double withdrawCash(double withdrawAmount) /*withdraw amount will be from user input, scanner variable*/
+    {
+        return withdrawAmount; //going to need if statements to confirm
+    }
+    public double depositCash(double depositAmount)
+    {
+        return depositAmount; //are if statements required for this method?
+    }
+    public double depositCheck(double depositAmount) //using same variable for deposit methods. 
+    //going to need some way to confirm if the amount on the check is correct. Like a real ATM does
+    {
+        return depositAmount; /*both deposit methods need to reset depositAmount variable so that
+                                they could deposit again in the future*/
+    }
+    
 }
+
