00f7e1f4dce1f4ea342a3f366a296a54bc9903bb
Alex Hernandez <alexish4@cpp.edu>
2021-10-08 20:02:01 -0700

    Added Shivam's changes

--- SkeletonCode.java
+++ SkeletonCode.java
@@ -1,11 +1,42 @@
-public class SkeletonCode 
+public class SkeletonCode
 {
+    boolean moneyTransfered;
     double withdrawAmount;
+    double totalFunds;
+    double remainingFunds = totalFunds - withdrawAmount;
     double depositAmount;
+
     public static void main(String[] args)
     {
 
     }
+   
+    /* User inputs the amount to be withdrawn and the accountID number for the recipient of the payment*/
+    public boolean transferFunds(double withdrawAmount, int accountID)
+    {
+        /* The withdrawnAmount is withdrawn from the sending account's totalFunds, leaving the remainingFunds*/
+        /* Needs if statement to determine if selected amount to transfer is present within account*/
+        /* AccountID is needed to determine who recieves the money and if recieving account exists*/
+        return moneyTransfered; 
+        /* Boolean T/F determines is amount was successfully transfered or not */
+    }
+   
+    /* User checks the Balane of their account*/
+    public double checkBalance()
+    {
+        return totalFunds;
+        /* Customer can check the balance of the Account*/
+        /* Since customers can have multiple accounts, must be able to differentiate between accounts*/
+    }
+   
+    /* User is able to print a reciept of transactions made on ATM Subsystem */
+    public void printReciept()
+    {
+        /* Nothing is returned since the user is recieveing a summary of their activities*/
+        /* printReciept would show the user every transaction they completed through the ATM Subsytem*/
+    }
+
+
     public double withdrawCash(double withdrawAmount) /*withdraw amount will be from user input, scanner variable*/
     {
         return withdrawAmount; //going to need if statements to confirm
@@ -22,3 +53,4 @@ public class SkeletonCode
     }
     
 }
+
